cf95cf14205ceb890b1b9eaa321e4cce6b9c1eb0
Timothy Jones <tjones@dius.com.au>
2018-12-22 10:05:00 +1100

    Refactor error handling

--- src/connectors/weatherForecast/index.js
+++ src/connectors/weatherForecast/index.js
@@ -4,20 +4,19 @@ const convert = require('xml-js');
 
 const MELBOURNE_WEATHER_URL = 'ftp://ftp.bom.gov.au/anon/gen/fwo/IDV10450.xml';
 
+const process = (inp, fn) => (inp.error ? inp : fn(inp));
+const makeError = (error, text) => ({ error, text });
+
 const getWeatherForecast = () =>
   new Promise((resolve, reject) => {
-    ftp.get(MELBOURNE_WEATHER_URL, 'melbourne.xml', function(err, res) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(res);
-      }
-    });
+    ftp.get(MELBOURNE_WEATHER_URL, 'melbourne.xml', (err, res) =>
+      err ? reject(err) : resolve(res)
+    );
   })
-    .then(
-      file => fs.readFileSync(file, { encoding: 'utf8' }),
-      error => ({ error, text: 'Network error' })
-    )
-    .then(xml => (xml.error ? xml : JSON.parse(convert.xml2json(xml))));
+    .catch(error => makeError(error, 'Network error'))
+    .then(file => process(file, f => fs.readFileSync(f, { encoding: 'utf8' })))
+    .catch(error => makeError(error, 'Unable to read file'))
+    .then(xml => process(xml, x => JSON.parse(convert.xml2json(x))))
+    .catch(error => makeError(error, 'Unable to parse file'));
 
 module.exports.getWeatherForecast = getWeatherForecast;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 const forecast = require('./connectors/weatherForecast');
 
-forecast.getWeatherForecast().then(forecast => console.log(forecast));
+forecast.getWeatherForecast().then(tomorrow => console.log(tomorrow));
