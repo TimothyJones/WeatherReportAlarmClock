9e13d2ca20115af403732198b65bd668cb901666
Timothy Jones <tjones@dius.com.au>
2019-07-08 17:02:56 +1000

    fix: Correct forecast selector

--- src/connectors/weatherForecast/converter.js
+++ src/connectors/weatherForecast/converter.js
@@ -1,30 +1,35 @@
-const moment = require('moment-timezone');
+const moment = require("moment-timezone");
 
 const forecastFor = (json, time) => {
-  const day = json.elements[0].elements[1].elements
-    .filter(a => a.attributes.aac === 'VIC_PT042')[0]
+  console.log(JSON.stringify(json.elements), time);
+  const day = json.elements[0].elements
+    .find(e => e.name === "forecast")
+    .elements.find(a => a.attributes.aac === "VIC_PT042")
     .elements.filter(
       a =>
-        moment(a.attributes['start-time-utc']).isBefore(time) &&
-        moment(a.attributes['end-time-utc']).isAfter(time)
+        moment(a.attributes["start-time-utc"]).isBefore(time) &&
+        moment(a.attributes["end-time-utc"]).isAfter(time)
     );
+
   if (day && day[0]) return day[0].elements;
   return null;
 };
 
-const textFromNode = node => (node && node[0] ? node[0].elements[0].text : undefined);
+const textFromNode = node =>
+  node && node[0] ? node[0].elements[0].text : undefined;
 
-const select = (forecast, text) => textFromNode(forecast.filter(a => a.attributes.type === text));
+const select = (forecast, text) =>
+  textFromNode(forecast.filter(a => a.attributes.type === text));
 
 const marshal = selectedForecast =>
   selectedForecast
     ? {
-        text: select(selectedForecast, 'precis'),
-        minTemp: select(selectedForecast, 'air_temperature_minimum'),
-        maxTemp: select(selectedForecast, 'air_temperature_maximum'),
-        rainChance: select(selectedForecast, 'probability_of_precipitation')
+        text: select(selectedForecast, "precis"),
+        minTemp: select(selectedForecast, "air_temperature_minimum"),
+        maxTemp: select(selectedForecast, "air_temperature_maximum"),
+        rainChance: select(selectedForecast, "probability_of_precipitation")
       }
-    : { error: 'No day selected', text: 'the selected day was missing.' };
+    : { error: "No day selected", text: "the selected day was missing." };
 
 const convertForecast = json => marshal(forecastFor(json, moment().utc()));
 
