c5386889be89cdf9ea2b8c3a584c1e911737c8f5
Timothy Jones <tjones@dius.com.au>
2019-07-30 12:37:06 +1000

    chore: Code cleanup

--- lambdas/wrapWithChime/src/index.js
+++ lambdas/wrapWithChime/src/index.js
@@ -4,7 +4,7 @@ const chimes = require('./domain/chimes');
 module.exports.wrapFile = ({ bucket, key }) => {
   const s3 = s3connector({ bucket, key });
   return s3
-    .getObject(key)
+    .getObject()
     .then(chimes.wrap)
     .then(s3.putObject);
 };
